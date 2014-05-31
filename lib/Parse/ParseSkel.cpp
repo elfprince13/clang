37e29a7a1d1f65b83852a828f995d029fa0593ca
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2014-05-30 22:30:03 -0400

    Verified: Skeleton names trigger symbol lookup
    Also added skeletons to Driver/Types.def so that we don’t need to use
    the clang frontend directly. Currently generate null statements for
    skeletons.

--- lib/Parse/ParseSkel.cpp
+++ lib/Parse/ParseSkel.cpp
@@ -12,6 +12,7 @@
 #include "clang/Parse/ParseDiagnostic.h"
 #include "clang/Sema/DeclSpec.h"
 #include "clang/Sema/PrettyDeclStackTrace.h"
+#include "clang/Sema/SemaDiagnostic.h"
 #include "clang/Sema/Scope.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringExtras.h"
@@ -25,17 +26,34 @@ Parser::DeclGroupPtrTy Parser::ParseTopLevelSkeleton() {
 
 
 Parser::StmtResult Parser::ParseSkeleton(SourceLocation AtLoc){
+	StmtResult ret = StmtError();
 	if (Tok.is(tok::identifier)) {
 		IdentifierInfo *ii = Tok.getIdentifierInfo();
 		SkeletonHandler handler = GetHandlerForSkeleton(*ii);
-		return (handler == nullptr) ? StmtError() : handler(ConsumeToken());
+		if (handler == nullptr) {
+			// TODO: this blows up if uncommented, but I can't even step
+			// in with the debugger for some reason to see why.
+			// I think I must need access to a Sema or something.
+			//Diag(Tok, diag::err_undeclared_var_use);
+		} else {
+			ret = handler(ConsumeToken());
+		}
 	} else {
-		return StmtError();
+		Diag(Tok, diag::err_unexpected_at);
 	}
+	return ret;
+}
+
+
+Parser::StmtResult doParseSkeletonWithName_Loop2dAccumulate(SourceLocation kindLoc){
+	return StmtResult();
 }
 
 Parser::SkeletonHandler Parser::GetHandlerForSkeleton(IdentifierInfo &skelIdent){
-	std::string symName("doParseSkeletonWithName-");
+	// Hard-code this as a test;
+	llvm::sys::DynamicLibrary::AddSymbol("doParseSkeletonWithName_Loop2dAccumulate", (void*)&doParseSkeletonWithName_Loop2dAccumulate);
+	
+	std::string symName("doParseSkeletonWithName_");
 	StringRef skelName = skelIdent.getName();
 	symName += skelName;
 	
